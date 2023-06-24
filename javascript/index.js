ee72578724af55d798e85b5b924fca9ca91af6a3
Apostolos <apostolos052@gmail.com>
2023-06-24 22:42:17 +0300

    changes

---
+++ javascript/index.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('games');
+const naturalWidth = button.naturalWidth;
+const naturalHeight = button.naturalHeight;
+console.log(`button natural width: ${naturalWidth}px`);
+console.log(`button natural height: ${naturalHeight}px`);
