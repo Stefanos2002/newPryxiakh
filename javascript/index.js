d6b8e97774a6a295484b6825e36170bbdbce7bf3
Stefanos2002 <kalouliss@gmail.com>
2023-06-25 13:36:34 +0300

    some js

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,30 @@
-const button = document.getElementById('games');
+const button = document.getElementById("games");
 const naturalWidth = button.naturalWidth;
 const naturalHeight = button.naturalHeight;
 console.log(`button natural width: ${naturalWidth}px`);
 console.log(`button natural height: ${naturalHeight}px`);
+
+const darkKnight = document.getElementById("dark-knight");
+const Senua = document.getElementById("senua");
+
+darkKnight.addEventListener("mouseenter", function () {
+  darkKnight.style.transform = "scale(1.1)";
+  Senua.style.transform = "scaleX(0.9)";
+  Senua.style.transformOrigin = "right";
+});
+
+darkKnight.addEventListener("mouseleave", function () {
+  Senua.style.transform = "scaleX(1)";
+  darkKnight.style.transform = "scale(1)";
+});
+
+Senua.addEventListener("mouseenter", function () {
+  Senua.style.transform = "scale(1.1)";
+  darkKnight.style.transform = "scaleX(0.9)";
+  darkKnight.style.transformOrigin = "left";
+});
+
+Senua.addEventListener("mouseleave", function () {
+  darkKnight.style.transform = "scaleX(1)";
+  Senua.style.transform = "scale(1)";
+});
