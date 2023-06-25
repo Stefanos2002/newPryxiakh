1a482c10f86b63b70afaf0d0af4d82123b1db3c8
Apostolos <apostolos052@gmail.com>
2023-06-25 17:43:03 +0300

    video

--- javascript/index.js
+++ javascript/index.js
@@ -1,24 +1,13 @@
-const darkKnight = document.getElementById("dark-knight");
-const Senua = document.getElementById("senua");
+const video = document.getElementById("my-video");
+const poster = document.getElementById("dark-knight");
+const videoContainer = document.getElementById("movies");
 
-darkKnight.addEventListener("mouseenter", function () {
-  darkKnight.style.transform = "scale(1.1)";
-  Senua.style.transform = "scaleX(0.9)";
-  Senua.style.transformOrigin = "right";
+videoContainer.addEventListener("mouseenter", function () {
+  video.play();
+  poster.style.display = "none"; // Hide the poster image
 });
 
-darkKnight.addEventListener("mouseleave", function () {
-  Senua.style.transform = "scaleX(1)";
-  darkKnight.style.transform = "scale(1)";
-});
-
-Senua.addEventListener("mouseenter", function () {
-  Senua.style.transform = "scale(1.1)";
-  darkKnight.style.transform = "scaleX(0.9)";
-  darkKnight.style.transformOrigin = "left";
-});
-
-Senua.addEventListener("mouseleave", function () {
-  darkKnight.style.transform = "scaleX(1)";
-  Senua.style.transform = "scale(1)";
+videoContainer.addEventListener("mouseleave", function () {
+  video.pause();
+  poster.style.display = "block"; // Show the poster image
 });
