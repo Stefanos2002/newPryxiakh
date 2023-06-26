1f221e3a054287cdc7d9e2396931d00b83be1559
Apostolos <apostolos052@gmail.com>
2023-06-26 16:44:57 +0300

    testing new idea

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,24 @@
 const video = document.getElementById("video1");
 const poster = document.getElementById("senua");
 const videoContainer = document.getElementById("games");
+const clip = document.querySelectorAll('clip');
 
 videoContainer.addEventListener("mouseenter", function () {
   video.play();
-  poster.style.display = "none"; // Hide the poster image
+  poster.style.display = "none";
 });
 
 videoContainer.addEventListener("mouseleave", function () {
   video.pause();
-  poster.style.display = "block"; // Show the poster image
+  poster.style.display = "block";
 });
+
+for (let i = 0; i < clip.length; i++) {
+  clip[i].addEventListener('mouseenter', () => {
+    clip[i].play();
+  })
+  clip[i].addEventListener('mouseleave', () => {
+    clip[i].pause();
+  })
+
+}
