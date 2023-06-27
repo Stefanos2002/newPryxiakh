4dd10864b02044d708397e5a3ca803985168b5a6
Apostolos <apostolos052@gmail.com>
2023-06-27 22:56:09 +0300

    made some changes

--- javascript/index.js
+++ javascript/index.js
@@ -1,21 +1,27 @@
 const video = document.getElementById("video1");
-const poster = document.getElementById("senua");
+const poster = document.querySelector(".senua");
 const videoContainer = document.getElementById("games");
 const clip = document.querySelectorAll(".clip");
 let isPlaying = false;
 
-for (let i = 0; i < clip.length; i++) {
-  clip[i].addEventListener("mouseenter", () => {
-    isPlaying = true;
-    setTimeout(function() {
-      if(isPlaying === true)
-        clip[i].play();
-    }, 650);
-    clip[i].volume = 0.1;
-  });
+poster.addEventListener('mouseenter', () => {
+  poster.style.transition = '0.3s ease-in-out'
+  poster.style.visibility = 'hidden'
+  for (let i = 0; i < clip.length; i++) {
+    clip[i].addEventListener("mouseenter", () => {
+      isPlaying = true;
+      setTimeout(function () {
+        if (isPlaying === true)
+          clip[i].play();
+      }, 650);
+      clip[i].volume = 0.1;
+    });
 
-  clip[i].addEventListener("mouseleave", () => {
-    isPlaying = false;
-    clip[i].pause();
-  });
-}
+    clip[i].addEventListener("mouseleave", () => {
+      isPlaying = false;
+      clip[i].pause();
+    });
+  }
+
+})
+
