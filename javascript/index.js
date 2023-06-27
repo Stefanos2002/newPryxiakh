efd63936d945dddfb66486d3846cb570acf5814c
Apostolos <apostolos052@gmail.com>
2023-06-27 23:11:32 +0300

    made some changes

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,13 @@
 const video = document.getElementById("video1");
-const poster = document.querySelector(".senua");
+const senuaPoster = document.querySelector(".senua");
+const batmanPoster = document.getElementById('dark-knight');
 const videoContainer = document.getElementById("games");
 const clip = document.querySelectorAll(".clip");
 let isPlaying = false;
 
-poster.addEventListener('mouseenter', () => {
-  poster.style.transition = '0.5s ease-in-out'
-  poster.style.visibility = 'hidden'
+senuaPoster.addEventListener('mouseenter', () => {
+  senuaPoster.style.transition = '0.8s ease-in-out'
+  senuaPoster.style.visibility = 'hidden'
   for (let i = 0; i < clip.length; i++) {
     clip[i].addEventListener("mouseenter", () => {
       isPlaying = true;
@@ -22,6 +23,24 @@ poster.addEventListener('mouseenter', () => {
       clip[i].pause();
     });
   }
-
 })
 
+batmanPoster.addEventListener('mouseenter', () => {
+  batmanPoster.style.transition = '0.8s ease-in-out'
+  batmanPoster.style.visibility = 'hidden'
+  for (let i = 0; i < clip.length; i++) {
+    clip[i].addEventListener("mouseenter", () => {
+      isPlaying = true;
+      setTimeout(function () {
+        if (isPlaying === true)
+          clip[i].play();
+      }, 650);
+      clip[i].volume = 0.1;
+    });
+
+    clip[i].addEventListener("mouseleave", () => {
+      isPlaying = false;
+      clip[i].pause();
+    });
+  }
+})
