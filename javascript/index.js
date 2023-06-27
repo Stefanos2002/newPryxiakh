224b76d77440b953a2d7fe24b42d67094be86fd4
Stefanos2002 <kalouliss@gmail.com>
2023-06-27 16:32:22 +0300

    αλλαγες

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,13 @@
 const video = document.getElementById("video1");
 const poster = document.getElementById("senua");
 const videoContainer = document.getElementById("games");
-const clip = document.querySelectorAll('clip');
+const clip = document.querySelectorAll("clip");
 
 videoContainer.addEventListener("mouseenter", function () {
-  video.play();
+  setTimeout(function () {
+    video.play();
+  }, 650);
+  video.volume = 0.1;
   poster.style.display = "none";
 });
 
@@ -14,11 +17,10 @@ videoContainer.addEventListener("mouseleave", function () {
 });
 
 for (let i = 0; i < clip.length; i++) {
-  clip[i].addEventListener('mouseenter', () => {
+  clip[i].addEventListener("mouseenter", () => {
     clip[i].play();
-  })
-  clip[i].addEventListener('mouseleave', () => {
+  });
+  clip[i].addEventListener("mouseleave", () => {
     clip[i].pause();
-  })
-
+  });
 }
