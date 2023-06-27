6bc36784eb33007bdcaea00b6d62c39f8e75828f
Apostolos <apostolos052@gmail.com>
2023-06-27 23:20:23 +0300

    made some changes

--- javascript/index.js
+++ javascript/index.js
@@ -4,43 +4,40 @@ const batmanPoster = document.getElementById('dark-knight');
 const videoContainer = document.getElementById("games");
 const clip = document.querySelectorAll(".clip");
 let isPlaying = false;
+console.log(isPlaying)
 
 senuaPoster.addEventListener('mouseenter', () => {
-  senuaPoster.style.transition = '0.8s ease-in-out'
-  senuaPoster.style.visibility = 'hidden'
-  for (let i = 0; i < clip.length; i++) {
-    clip[i].addEventListener("mouseenter", () => {
-      isPlaying = true;
-      setTimeout(function () {
-        if (isPlaying === true)
-          clip[i].play();
-      }, 650);
-      clip[i].volume = 0.1;
-    });
-
-    clip[i].addEventListener("mouseleave", () => {
-      isPlaying = false;
-      clip[i].pause();
-    });
-  }
+  handleMouseEnter(senuaPoster);
 })
 
 batmanPoster.addEventListener('mouseenter', () => {
-  batmanPoster.style.transition = '0.8s ease-in-out'
-  batmanPoster.style.visibility = 'hidden'
+  handleMouseEnter(batmanPoster);
+})
+
+
+//Function gia ton elegxo tou hover stis eikones
+function handleMouseEnter(poster) {
+  poster.style.transition = "0.8s ease-in-out";
+  poster.style.visibility = "hidden";
+
   for (let i = 0; i < clip.length; i++) {
     clip[i].addEventListener("mouseenter", () => {
       isPlaying = true;
+      console.log(isPlaying)
       setTimeout(function () {
-        if (isPlaying === true)
+        if (isPlaying === true) {
           clip[i].play();
+        }
       }, 650);
       clip[i].volume = 0.1;
     });
 
     clip[i].addEventListener("mouseleave", () => {
       isPlaying = false;
+      console.log(isPlaying)
       clip[i].pause();
     });
   }
-})
+}
+
+
