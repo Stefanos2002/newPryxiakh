2d009d0c14bfff43dc1024588f1384f4418fadb0
Stefanos2002 <kalouliss@gmail.com>
2023-06-28 12:33:42 +0300

    fixed

--- javascript/index.js
+++ javascript/index.js
@@ -1,26 +1,40 @@
 const video = document.getElementById("video1");
-const poster = document.getElementById("senua");
+const senuaPoster = document.querySelector(".senua");
+const batmanPoster = document.getElementById("dark-knight");
 const videoContainer = document.getElementById("games");
-const clip = document.querySelectorAll("clip");
+const clip = document.querySelectorAll(".clip");
+let isPlaying = false;
+console.log(isPlaying);
 
-videoContainer.addEventListener("mouseenter", function () {
-  setTimeout(function () {
-    video.play();
-  }, 650);
-  video.volume = 0.1;
-  poster.style.display = "none";
+senuaPoster.addEventListener("mouseenter", () => {
+  handleMouseEnter(senuaPoster);
 });
 
-videoContainer.addEventListener("mouseleave", function () {
-  video.pause();
-  poster.style.display = "block";
+batmanPoster.addEventListener("mouseenter", () => {
+  handleMouseEnter(batmanPoster);
 });
 
-for (let i = 0; i < clip.length; i++) {
-  clip[i].addEventListener("mouseenter", () => {
-    clip[i].play();
-  });
-  clip[i].addEventListener("mouseleave", () => {
-    clip[i].pause();
-  });
+//Function gia ton elegxo tou hover stis eikones
+function handleMouseEnter(poster) {
+  poster.style.transition = "0.8s ease-in-out";
+  poster.style.visibility = "hidden";
+
+  for (let i = 0; i < clip.length; i++) {
+    clip[i].addEventListener("mouseenter", () => {
+      isPlaying = true;
+      console.log(isPlaying);
+      setTimeout(function () {
+        if (isPlaying === true) {
+          clip[i].play();
+        }
+      }, 650);
+      clip[i].volume = 1;
+    });
+
+    clip[i].addEventListener("mouseleave", () => {
+      isPlaying = false;
+      console.log(isPlaying);
+      clip[i].pause();
+    });
+  }
 }
