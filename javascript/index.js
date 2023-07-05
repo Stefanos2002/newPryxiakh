7a64565d73fb5e60055ba4a7facc036b377df6e9
Stefanos2002 <kalouliss@gmail.com>
2023-07-05 13:35:12 +0300

    FIxingVideos

--- javascript/index.js
+++ javascript/index.js
@@ -1,18 +1,38 @@
+/*TODO
+1) Na paizei to video sto hover sto text 
+2) Na mhn paizei to video sthn grigori enallagh apo poster se poster*/
+
 //HTML elements
 const senuaPoster = document.querySelector(".senua");
 const batmanPoster = document.querySelector(".dark-knight");
 const videoContainer = document.getElementById("games");
-const gamesText = document.getElementById("games-text");
-const moviesText = document.getElementById("movies-text");
+const clipText = document.querySelectorAll('.clip-text')
 const clip = document.querySelectorAll(".clip");
+let hoverImages;
+let checker = false;
 
+window.addEventListener('DOMContentLoaded', function() { //giati evgaze error sto poster.style.transition gia kapoio logo
 senuaPoster.addEventListener("mouseenter", () => {
   handleMouseEnter(senuaPoster);
+});
+senuaPoster.addEventListener("mouseleave", () => {
+  this.clearTimeout(hoverImages);
 });
 
 batmanPoster.addEventListener("mouseenter", () => {
   handleMouseEnter(batmanPoster);
 });
+
+//new aspects
+batmanPoster.onmouseover = function () {
+  window.mytimeout = setTimeout(function() {
+    handleMouseEnter(poster);    
+  },2000);
+};
+
+batmanPoster.onmouseout = function() {
+    clearTimeout(window.mytimeout);
+};
 
 //Function gia ton elegxo tou hover stis eikones
 function handleMouseEnter(poster) {
@@ -22,12 +42,40 @@ function handleMouseEnter(poster) {
     poster.style.pointerEvents = "none";
   }, 650);
 
+
   for (let i = 0; i < clip.length; i++) {
     let isMouseOver = false;
     let hoverTimeout;
 
+
     clip[i].addEventListener("mouseover", () => {
+
       isMouseOver = true;
+      clipText[i].onmouseover = () => {
+        clip[i].style.transition = '1s ease-in-out'
+
+        //DEN TELEIWSE AKOMA
+        setTimeout(() => {
+          if(isMouseOver === true) {
+            clip[i].play(); //Gia na paizoun ta video sto hover sto text
+          }
+        }, 530)
+      }
+
+      clipText[i].onmouseover = () => {
+        clip[i].style.transition = '1s ease-in-out'
+
+        //DEN TELEIWSE AKOMA
+        setTimeout(() => {
+          if(isMouseOver === true) {
+
+            clip[i].play(); //Gia na paizoun ta video sto hover sto text
+          }
+          
+
+        }, 530)
+      }
+
       setTimeout(function () {
         if (isMouseOver === true) {
           clip[i].play();
@@ -42,3 +90,4 @@ function handleMouseEnter(poster) {
     });
   }
 }
+});
