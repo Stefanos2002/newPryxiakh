bf08f22aede78f351584d0c11db4476ce60c306c
unknown <apostolos052@gmail.com>
2023-07-05 23:25:36 +0300

    fixed text-hover

--- javascript/index.js
+++ javascript/index.js
@@ -5,92 +5,67 @@
 //HTML elements
 const senuaPoster = document.querySelector(".senua");
 const batmanPoster = document.querySelector(".dark-knight");
-const videoContainer = document.getElementById("games");
 const clipText = document.querySelectorAll('.clip-text')
 const clip = document.querySelectorAll(".clip");
+const videoContainer = document.querySelectorAll(".video-container");
+
+//Metavlhtes
 let hoverImages;
-let checker = false;
 
-window.addEventListener('DOMContentLoaded', function() { //giati evgaze error sto poster.style.transition gia kapoio logo
-senuaPoster.addEventListener("mouseenter", () => {
-  handleMouseEnter(senuaPoster);
-});
-senuaPoster.addEventListener("mouseleave", () => {
-  this.clearTimeout(hoverImages);
-});
 
-batmanPoster.addEventListener("mouseenter", () => {
-  handleMouseEnter(batmanPoster);
-});
 
-//new aspects
-batmanPoster.onmouseover = function () {
-  window.mytimeout = setTimeout(function() {
-    handleMouseEnter(poster);    
-  },2000);
-};
+window.addEventListener('DOMContentLoaded', function () { //giati evgaze error sto poster.style.transition gia kapoio logo
+  senuaPoster.addEventListener("mouseenter", () => {
+    handleMouseEnter(senuaPoster);
+  });
+  senuaPoster.addEventListener("mouseleave", () => {
+    this.clearTimeout(hoverImages);
+  });
 
-batmanPoster.onmouseout = function() {
+  batmanPoster.addEventListener("mouseenter", () => {
+    handleMouseEnter(batmanPoster);
+  });
+
+  //new aspects
+  batmanPoster.onmouseover = function () {
+    window.mytimeout = setTimeout(function () {
+      handleMouseEnter(poster);
+    }, 2000);
+  };
+
+  batmanPoster.onmouseout = function () {
     clearTimeout(window.mytimeout);
-};
-
-//Function gia ton elegxo tou hover stis eikones
-function handleMouseEnter(poster) {
-  poster.style.transition = "1s ease-in-out";
-  setTimeout(() => {
-    poster.style.opacity = "0";
-    poster.style.pointerEvents = "none";
-  }, 650);
+  };
 
 
+  //Function gia ton elegxo tou hover stis eikones
+  function handleMouseEnter(poster) {
+    poster.style.transition = "1s ease-in-out";
+    setTimeout(() => {
+      poster.style.opacity = "0";
+      poster.style.pointerEvents = "none";
+    }, 650);
 
-  for (let i = 0; i < clip.length; i++) {
-    let isMouseOver = false;
-    let hoverTimeout;
 
+    for (let i = 0; i < videoContainer.length; i++) {
+      let isMouseOver = false;
+      let hoverTimeout;
 
+      videoContainer[i].addEventListener("mouseover", () => {
 
-    clip[i].addEventListener("mouseover", () => {
-
-
-      isMouseOver = true;
-      clipText[i].onmouseover = () => {
-        clip[i].style.transition = '1s ease-in-out'
-
-        //DEN TELEIWSE AKOMA
-        setTimeout(() => {
-          if(isMouseOver === true) {
-            clip[i].play(); //Gia na paizoun ta video sto hover sto text
+        isMouseOver = true;
+        setTimeout(function () {
+          if (isMouseOver === true) {
+            clip[i].play();
           }
-        }, 530)
-      }
+        }, 530);
+      });
 
-      clipText[i].onmouseover = () => {
-        clip[i].style.transition = '1s ease-in-out'
-
-        //DEN TELEIWSE AKOMA
-        setTimeout(() => {
-          if(isMouseOver === true) {
-
-            clip[i].play(); //Gia na paizoun ta video sto hover sto text
-          }
-          
-
-        }, 530)
-      }
-
-      setTimeout(function () {
-        if (isMouseOver === true) {
-          clip[i].play();
-        }
-      }, 530);
-    });
-
-    clip[i].addEventListener("mouseleave", () => {
-      isMouseOver = false;
-      clearTimeout(hoverTimeout);
-      clip[i].pause();
-    });
+      videoContainer[i].addEventListener("mouseleave", () => {
+        isMouseOver = false;
+        clearTimeout(hoverTimeout);
+        clip[i].pause();
+      });
+    }
   }
-}
 });
