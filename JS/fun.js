7e8a59d22d4f5535d35a666380b0e7eabd4e521e
Stefanos2002 <kalouliss@gmail.com>
2023-07-20 17:30:42 +0300

    UpdateFixes

--- JS/fun.js
+++ JS/fun.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const x = document.getElementById("xmen");
   const input = document.getElementById("bar");
   const drop = document.getElementById("list");
+  const controller = document.getElementById("controller-img");
+  const options = drop.options;
 
   //appear th X icon when something is written something
   input.addEventListener("input", function () {
@@ -19,5 +21,12 @@ document.addEventListener("DOMContentLoaded", () => {
       input.style.width = "35vw";
     }
   });
-  drop.addEventListener("mouseover", function () {});
+  drop.addEventListener("mouseover", function () {
+    drop.style.width = "130px";
+  });
+  drop.addEventListener("mouseleave", function () {
+    drop.style.width = "25px";
+    //closes down the dropdown list
+    drop.blur();
+  });
 });
