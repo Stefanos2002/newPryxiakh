f708a9ef89869dbc89705c31f18868bcaca6105b
unknown <apostolos052@gmail.com>
2023-07-27 18:12:46 +0300

    Dark-theme toggle button done

--- javascript/darkTheme.js
+++ javascript/darkTheme.js
@@ -1,6 +1,66 @@
 const theme = document.querySelector('.theme');
 const btn = document.querySelector('.button');
+const icon = document.querySelector('.button-icon');
+const body = document.querySelector('body');
+const navBar = document.querySelector('.nav-bar')
+let darkTheme = false;
 
-btn.addEventListener('click', ()=> {
+//function gia na apothikeuei to dark mode topika
+function store(value) {
+    localStorage.setItem('dark-mode', value);
+}
+
+
+const darkmode = localStorage.getItem('dark-mode');
+//An to darkmode den exei energopoihthei akoma
+if (darkmode === null) {
+    store(false);
+    icon.classList.add('fa-sun');   //icons tou font-awesome
+}
+//Epeidh xrhsimopoiw localStorage to true kai to false gyrnane ws strings
+//An to darkmode exei energopoihthei
+else if (darkmode === 'true') {
+    theme.classList.add('dark-mode');
+    icon.classList.add('fa-moon')
+}
+//An to darkmode exei energopoihthei alla to dark theme einai apenergopoihmeno
+else
+    icon.classList.add('fa-sun')
+
+
+
+btn.addEventListener('click', () => {
     theme.classList.toggle('dark-mode');
+    icon.classList.add('animated');
+
+    store(theme.classList.contains('dark-mode'));
+
+    if (theme.classList.contains('dark-mode')) {
+        darkTheme = true;
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+    }
+    else {
+        darkTheme = false;
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    }
+
+    if (darkTheme === true) {
+        body.style.transition = '0.6s ease-in-out';
+        body.style.backgroundColor = '#2b2b2b';
+        body.style.color = '#ececec';
+        navBar.style.transition = '0.6s ease-in-out';
+        navBar.style.backgroundColor = '#141418'
+    }
+    else {
+        body.style.transition = '0.6s ease-in-out';
+        body.style.backgroundColor = 'white';
+        body.style.color = 'black';
+        navBar.style.transition = '0.6s ease-in-out';
+        navBar.style.backgroundColor = '#23232e'
+    }
+    setTimeout(() => {
+        icon.classList.remove('animated');
+    }, 500);
 });
