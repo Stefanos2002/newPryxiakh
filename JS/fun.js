c9209ebcad3fbd73fc6019e44e0fe5b9c070bc88
Stefanos2002 <kalouliss@gmail.com>
2023-10-05 00:02:04 +0300

    update

--- JS/fun.js
+++ JS/fun.js
@@ -1,26 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const x = document.getElementById("xmen");
+  const x = document.getElementById("xicon");
   const input = document.getElementById("bar");
-  const drop = document.getElementById("list");
   const controller = document.getElementById("controller-img");
+  const drop = document.getElementById("list");
   const options = drop.options;
 
+  input.style.width = "35vw";
+  x.style.display = "none";
   //appear th X icon when something is written something
   input.addEventListener("input", function () {
     if (input.value.length > 0) {
-      x.style.display = "inline";
+      controller.style.right = "32px";
+      x.style.display = "inline-block";
+      x.style.opacity = 1;
+      input.style.width = "37vw";
       // Clear the input field value
       x.addEventListener("click", function () {
         input.value = "";
-        x.style.display = "none";
+        x.style.opacity = 0;
+
         input.style.width = "35vw";
+        controller.style.right = "6px";
       });
-      input.style.width = "37vw";
     } else {
-      x.style.display = "none";
+      controller.style.right = "6px";
+      x.style.opacity = 0;
+
       input.style.width = "35vw";
     }
   });
+
   drop.addEventListener("mouseover", function () {
     drop.style.width = "132px";
   });
