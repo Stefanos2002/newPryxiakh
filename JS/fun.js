e3b5f59f2752fa1ce313d428928eec0b77d7e8a2
Stefanos2002 <kalouliss@gmail.com>
2023-10-10 14:54:44 +0300

    update

--- JS/fun.js
+++ JS/fun.js
@@ -5,28 +5,26 @@ document.addEventListener("DOMContentLoaded", () => {
   const drop = document.getElementById("list");
   const options = drop.options;
 
-  input.style.width = "35vw";
-  x.style.display = "none";
   //appear th X icon when something is written something
   input.addEventListener("input", function () {
     if (input.value.length > 0) {
-      controller.style.right = "32px";
       x.style.display = "inline-block";
       x.style.opacity = 1;
-      input.style.width = "37vw";
+      input.style.paddingRight = "20em";
       // Clear the input field value
       x.addEventListener("click", function () {
         input.value = "";
         x.style.opacity = 0;
 
-        input.style.width = "35vw";
+        //input.style.width = "28vw";
+
         controller.style.right = "6px";
       });
     } else {
       controller.style.right = "6px";
       x.style.opacity = 0;
 
-      input.style.width = "35vw";
+      // input.style.width = "28vw";
     }
   });
 
